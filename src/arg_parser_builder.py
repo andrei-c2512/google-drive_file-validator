556d9505fcc5c878110762a71f52f40a1e457b47
andrei ciobanu <angryfk13@gmail.com>
2025-03-28 09:49:35 -0400

    Added the config command. Should work as expected

--- src/arg_parser_builder.py
+++ src/arg_parser_builder.py
@@ -1,17 +1,11 @@
 import argparse
 from config_file_manager import ConfigFileManager
+from config_file_manager import process_config_expressions
 from file_filter import *
 
-def build_arg_parser():
-    parser = argparse.ArgumentParser(
-        description="Delete files depending on the lifetime you configured. The program only works on *common drives*",
-        epilog="Project done by Leafy Alex and Sheepherder Alex"
-    )
-    
-    manager = ConfigFileManager("res/config.config")
-     
-    subparser = parser.add_subparsers(dest="command" , required=True , help="Available commands")
 
+def build_validate_parser(subparser):
+    manager = ConfigFileManager("res/config.config")
 
     validate_parser = subparser.add_parser("validate" ,
                                            help="Validate the liftime of files in a specific drive folder")
@@ -33,11 +27,26 @@ def build_arg_parser():
                                  required=False)
 
     validate_parser.set_defaults(func=validate_a)
-
-    # do this later
+    return
+#
+def build_config_parser(subparser) -> None:
     config_parser = subparser.add_parser("config" , help="Set certain variables in the config file")
+    config_parser.add_argument("expressions" , nargs="*", help = "List of expressions")
+    config_parser.set_defaults(func=process_config_expressions)
+    return
+#
 
-
+def build_arg_parser():
+    parser = argparse.ArgumentParser(
+        description="Delete files depending on the lifetime you configured. The program only works on *common drives*",
+        epilog="Project done by Leafy Alex and Sheepherder Alex"
+    )
+    
+    manager = ConfigFileManager("res/config.config")
+     
+    subparser = parser.add_subparsers(dest="command" , required=True , help="Available commands")
+    build_validate_parser(subparser)
+    build_config_parser(subparser)
     return parser
 
     
--- src/config_file_manager.py
+++ src/config_file_manager.py
@@ -23,21 +23,52 @@ class ConfigFileManager:
                 
         return            
 
-    def set_var(self , name : str , new_val : str) -> None:
+    def set_var(self , name : str , new_val : str) -> bool:
 
         file_data : str = ""
+        found : bool = False
         with open(self.path , 'r+') as file:
             for line in file: 
                 if line.find(name) != -1:
                     line = f"{name}: {new_val}\n"
+                    found = True
+                    # we do not exit the loop because we still add every line of the config line in the string
 
                 file_data += line            
             #
         #
-
+        
+        if found == False:
+            logging.error(f"The variable {name} does not exist")
+            return False
+        
         with open(self.path, 'w') as file:
             file.write(file_data)
+        return True
+    def set_vars(self , args):
+        success_cnt = 0 
+        for expression in args:
+            string_data = expression.split('=',1)
+            if len(string_data) != 2:
+                continue
 
+            success_cnt += self.set_var(string_data[0], string_data[1]) 
+        #  
+        if success_cnt == 0:
+            logging.info("No changes occured to the config file")
+        elif success_cnt == len(args):
+            logging.info("All changes were saved succesfully")
+        else:
+            failed_ops = len(args) - success_cnt
+            plural = str(int(failed_ops != 1) )
+            if plural == "0":
+                plural = ""
+            else:
+                plural = "s"
+
+            logging.info(f"{failed_ops} operation{plural} failed. {success_cnt} succeeded.")
+        return
+    #
     def get_var(self , name : str) -> str:
         return self.file_vars[name]
     
@@ -45,3 +76,13 @@ class ConfigFileManager:
         for key , value in self.file_vars.items():
             logging.info(f"{key} : {value}")
         return
+    #
+   
+#
+
+# the arguments are interpreted as <VAR>=<VAL> expressions
+def process_config_expressions(args):
+    config_file = ConfigFileManager("./res/config.config")
+    config_file.set_vars(args.expressions)
+    return
+#
